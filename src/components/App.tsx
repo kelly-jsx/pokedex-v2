fec8dadaa42ad14e9db7279d1182d09c4759d46d
Kelly <50586093+kelly-jsx@users.noreply.github.com>
2023-02-20 12:01:08 +0100

    API map PokeCard component, styling

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,6 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
 import { Counter } from 'utils/Counter'
 import Hero from './Hero/Hero'
 import { SiteActions } from './SiteActions'
@@ -8,6 +11,35 @@ import { Footer } from './Footer'
 export default function App() {
   const ApiUrl = 'https://pokeapi.co/api/v2/'
 
+  const [allPokemons, setAllPokemons] = useState([])
+  const [selectedPokemon, setSelectedPokemon] = useState(null)
+
+  useEffect(() => {
+    fetchAllPokemons(0, 151)
+  }, [])
+
+  const fetchAllPokemons = async (offset, limit) => {
+    const res = await axios
+      .get(`${ApiUrl}pokemon?limit=${limit}&offset=${offset}`)
+      .catch((err) => console.log(err))
+    fetchPokemonData(res.data.results)
+  }
+
+  const fetchPokemonData = async (res) => {
+    const pokemonArr = []
+
+    await Promise.all(
+      res.map((pokemon) => {
+        return axios.get(`${ApiUrl}pokemon/${pokemon.name}`).then((res) => {
+          pokemonArr.push(res.data)
+        })
+      })
+    )
+
+    pokemonArr.sort((a, b) => (a.id > b.id ? 1 : b.id > a.id ? -1 : 0))
+
+    setAllPokemons(pokemonArr)
+  }
   return (
     <>
       <Hero />
@@ -17,14 +49,15 @@ export default function App() {
         <SiteActions />
         <div className="divider" />
         <div className="mx-4 mt-8 grid grid-cols-1 gap-4 pb-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-7">
-          <PokeCard />
-          <PokeCard />
-          <PokeCard />
-          <PokeCard />
-          <PokeCard />
-          <PokeCard />
-          <PokeCard />
-          <PokeCard />
+          {Object.keys(allPokemons).map((item) => (
+            <PokeCard
+              key={allPokemons[item].id}
+              index={allPokemons[item].id}
+              name={allPokemons[item].name}
+              image={allPokemons[item].sprites.other.dream_world.front_default}
+              type={allPokemons[item].types}
+            />
+          ))}
         </div>
         <div className="place-self-center">
           <div className="btn-group">
--- src/components/PokeCard.tsx
+++ src/components/PokeCard.tsx
@@ -3,30 +3,42 @@ import React from 'react'
 import { Type } from './Type'
 import { getTypeColor } from 'utils'
 
-export const PokeCard = () => {
+export const PokeCard = ({ index, name, image, type }) => {
+  let finalColor = []
+
+  if (type.length === 2) {
+    finalColor = [
+      getTypeColor(type[0].type.name),
+      getTypeColor(type[1].type.name)
+    ]
+  } else {
+    finalColor = [
+      getTypeColor(type[0].type.name),
+      getTypeColor(type[0].type.name)
+    ]
+  }
+
   return (
     <label htmlFor="info-modal">
       <div
         className="hover:scale-103 cursor-pointer  rounded-lg bg-base-300 text-center text-black shadow-lg transition ease-in-out hover:-translate-y-1.5"
         style={{
-          backgroundColor: getTypeColor('fire')
+          background: `linear-gradient(${finalColor[0]}, ${finalColor[1]})`
         }}
       >
         <div
           className="rounded-lg p-3"
           style={{
-            backgroundColor: 'rgba(255, 255, 255, 0.2)'
+            backgroundColor: 'rgba(255, 255, 255, 0.3)'
           }}
         >
-          <p className="text-2xl">#001</p>
-          <img
-            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/4.svg"
-            alt=""
-            className="mx-auto mt-5 h-60"
-          />
-          <p className="mt-5 text-xl font-bold capitalize">Charmander</p>
+          <p className="text-2xl">#{String(index).padStart(3, '0')}</p>
+          <img src={image} alt="" className="mx-auto mt-5 h-60" />
+          <p className="mt-5 text-xl font-bold capitalize">{name}</p>
           <div className="mt-5 flex justify-center gap-2">
-            <Type pokeType={'fire'} />
+            {type.map((type) => (
+              <Type pokeType={type.type.name} />
+            ))}
           </div>
         </div>
       </div>
